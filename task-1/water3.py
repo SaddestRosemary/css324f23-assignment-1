27fe7a05f504007b34a2fede688278ff4ce7e5c3
SaddestRosemary <gus.danupat@gmail.com>
2023-09-11 17:19:46 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,23 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    a, b, c = s
+    if a==4 and b==4:
+        return True
 
 def successors(s):
-    x, y, z = s
-    return []
+    max_cap = [8,5,3]
+
+    # i is parent and j is child
+    for i in range(3):
+        for j in range(3):
+            # no tranfer if move x to x
+            if i != j :
+                part_cost = min(s[i],max_cap[j] - s[j])
+                if part_cost != 0:
+                    ans = list(s)
+                    ans[i] -= part_cost
+                    ans[j] += part_cost
+                    yield (tuple([ans[k] for k in range(3)]),part_cost)
+
